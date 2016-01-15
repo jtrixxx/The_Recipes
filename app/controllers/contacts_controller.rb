38a3b0055ad2483df5f6431e0c1ba11f4109facd
jtrixxx <michaelmathias6750@gmail.com>
2016-01-15 12:21:16 +0100

    Added and completed action mailer form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,10 +7,15 @@ class ContactsController < ApplicationController
 		@contact = Contact.new(contact_params)
 
 		if @contact.save
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+
+			ContactMailer.contact_email(name, email, body).deliver
 			flash[:success] = "Message Sent"
 			redirect_to new_contact_path
 		else
-			flash[:danger] = "Error occure, Message has not been Sent"
+			flash[:danger] = "Error occured, Message has not been Sent"
 			redirect_to new_contact_path
 		end
 	end
