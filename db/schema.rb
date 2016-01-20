7a2642ec7655faccf8749dd2c826f643b4570000
jtrixxx <michaelmathias6750@gmail.com>
2016-01-20 13:34:18 +0100

    Added chefs and recipes tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112092927) do
+ActiveRecord::Schema.define(version: 20160119140737) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.text     "comments"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
